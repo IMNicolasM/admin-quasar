e70887e169b2a5eb1e3b605686288b1487c13063
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2024-06-21 17:20:54 -0500

    Fixed: some logic and added url to .net service

--- src/setup/app.js
+++ src/setup/app.js
@@ -7,7 +7,7 @@ tagsToParceHost.forEach(tagToReplace => host = host.replace(tagToReplace, ''));
 let loadFrom = host.split('/')[1];
 
 export default {
-  baseUrl: 'https://n8ndtm.eastus.cloudapp.azure.com/webhook/78ccce39-3ea4-4dd1-bdf2-ef965be48350',
+  baseUrl: 'https://app-dtm-backend-dev-eus.azurewebsites.net',
   version: pjson.version,//Package version
   mode: (loadFrom && (['iadmin', 'ipanel'].indexOf(loadFrom) != -1)) ? loadFrom : 'iadmin',//Define load mode
   validateModeAccess: true,//Define if validate app mode access
